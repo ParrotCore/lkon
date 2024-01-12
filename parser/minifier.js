e8c8b234477c34d632a0c72590c9039207aa69ef
parrotcore <patrykbanasiak14@gmail.com>
2024-01-12 15:04:33 +0100

    LKON v3.0.0-4 - fixed the problem with multi-line comments removing;

--- parser/minifier.js
+++ parser/minifier.js
@@ -1,8 +1,34 @@
+function commentRemover(str)
+{
+    if(!/'''|#/.test(str)) return str;
+
+    let insideString,
+        insideRegex,
+        insideComment,
+        res = str.replace(/\s*#[^\r\n]*/g, '').split("");
+
+    for(let i = 0; i < res.length; i++)
+    {
+        if(res[i] == '"' && res[i-1] != '\\' && !insideRegex && !insideComment) insideString = !insideString;
+        else if(res[i] == '/' && res[i-1] != '\\' && !insideString && !insideComment) insideRegex = !insideRegex;
+        else if(res[i] + res[i+1] + res[i+2] == "'''" && res[i-1] != '\\' && !insideString && !insideRegex)
+        {
+            insideComment = !insideComment;
+
+            let j = i;
+            for(; j <= i + 2; j++) res[j] = "to_remove";
+            i = j-1;
+        }
+        else if(insideComment) res[i] = "to_remove";
+    }
+    
+    return res.filter(el => el != "to_remove").join("");
+}
+
 module.exports = function minify(str)
 {
     let 
-        replaced = str
-            .replace(/(#[^\n\r]*|'''[^]+''')(?=(?:[^"]*"[^"]*")*[^"]*$)/g, "")
+        replaced = commentRemover(str)
             .replace(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/g, ""),
         splitPoints = [0],
         insideString,
